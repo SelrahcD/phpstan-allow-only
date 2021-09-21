b1153de7cdda00ca4abd9e1b02197cf68fb0cf44
Ondrej Mirtes <ondrej@mirtes.cz>
2021-09-21 11:31:51 +0200

    Logic simplification

--- src/Rules/AllowOnlyFromRule.php
+++ src/Rules/AllowOnlyFromRule.php
@@ -10,13 +10,10 @@ use PHPStan\PhpDocParser\Ast\PhpDoc\GenericTagValueNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocChildNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTagNode;
+use PHPStan\Reflection\MethodReflection;
 use PHPStan\Rules\Rule;
 use PHPStan\Rules\RuleErrorBuilder;
 use PHPStan\Type\FileTypeMapper;
-use PHPStan\Type\ObjectType;
-use PHPStan\Type\ThisType;
-use PHPStan\Type\Type;
-use PHPStan\Type\UnionType;
 
 /**
  * @implements Rule<Node\Expr\MethodCall>
@@ -40,40 +37,40 @@ final class AllowOnlyFromRule implements Rule
 
     public function processNode(Node $node, Scope $scope): array
     {
-        $caller = $scope->getType($node->var);
+        $calledOnObject = $scope->getType($node->var);
         $methodIdentifier = $node->name;
         if (!$methodIdentifier instanceof Node\Identifier) {
             return [];
         }
 
         $methodName = $methodIdentifier->name;
-
-        if ($caller instanceof UnionType) {
-            return array_reduce($caller->getTypes(), function (array $errors, Type $type) use ($methodName, $scope) {
-                return array_merge($errors, $this->ensureMethodCanBeCalledFromHere($type, $methodName, $scope));
-            }, []);
+        if (!$calledOnObject->hasMethod($methodName)->yes()) {
+            return [];
         }
 
-        return $this->ensureMethodCanBeCalledFromHere($caller, $methodName, $scope);
+        return $this->ensureMethodCanBeCalledFromHere($calledOnObject->getMethod($methodName, $scope), $scope);
     }
 
     /**
      * @return string[]
      */
-    private function listAllowedCallers(ObjectType $caller, string $methodName, Scope $scope): array
+    private function listAllowedCallers(MethodReflection $method): array
     {
-        if (
-            $caller->getMethod($methodName, $scope)->getDocComment() === null
-        ) {
+        if ($method->getDocComment() === null) {
+            return [];
+        }
+
+        $fileName = $method->getDeclaringClass()->getFileName();
+        if ($fileName === false) {
             return [];
         }
 
         $resolvedPhpDoc = $this->fileTypeMapper->getResolvedPhpDoc(
-            $caller->getClassReflection()->getFileName(),
-            $caller->getClassName(),
+            $fileName,
+            $method->getDeclaringClass()->getName(),
             null,
-            $methodName,
-            $caller->getMethod($methodName, $scope)->getDocComment()
+            $method->getName(),
+            $method->getDocComment()
         );
 
         $phpDocNodes = $resolvedPhpDoc->getPhpDocNodes();
@@ -93,7 +90,7 @@ final class AllowOnlyFromRule implements Rule
     /**
      * @param string[] $authorizedCallers
      */
-    private function errorMessage(string $className, string $methodName, array $authorizedCallers): \PHPStan\Rules\RuleError
+    private function errorMessage(MethodReflection $methodReflection, array $authorizedCallers): \PHPStan\Rules\RuleError
     {
         $authorizedCallersAsString = array_reduce($authorizedCallers, function (string $authorizedCallersAsString, string $authorizedCaller) {
             $authorizedCallersAsString .= PHP_EOL . '- ' . $authorizedCaller;
@@ -101,24 +98,18 @@ final class AllowOnlyFromRule implements Rule
             return $authorizedCallersAsString;
         }, '');
 
-        return RuleErrorBuilder::message(sprintf('Call to %s::%s is authorized only from:%s', $className, $methodName, $authorizedCallersAsString))->build();
+        return RuleErrorBuilder::message(sprintf('Call to %s::%s is authorized only from:%s', $methodReflection->getDeclaringClass()->getName(), $methodReflection->getName(), $authorizedCallersAsString))->build();
     }
 
     /**
-     * @return array|\PHPStan\Rules\RuleError[]
+     * @return \PHPStan\Rules\RuleError[]
      */
-    private function ensureMethodCanBeCalledFromHere(Type $caller, string $methodName, Scope $scope): array
+    private function ensureMethodCanBeCalledFromHere(MethodReflection $methodReflection, Scope $scope): array
     {
-        // Always allow calls from inside the class
-        if ($caller instanceof ThisType) {
+        $allowedCallers = $this->listAllowedCallers($methodReflection);
+        if ($scope->isInClass() && $methodReflection->getDeclaringClass()->getName() === $scope->getClassReflection()->getName()) {
             return [];
         }
-
-        if (!$caller instanceof ObjectType) {
-            return [];
-        }
-
-        $allowedCallers = $this->listAllowedCallers($caller, $methodName, $scope);
 
         // Because it doesn't have allowed callers we know the tag is not set.
         // It means we don't have to apply the rule for that method call.
@@ -130,7 +121,7 @@ final class AllowOnlyFromRule implements Rule
         // made outside one of allowed callers
         if (!$scope->isInClass()) {
             return [
-                $this->errorMessage($caller->getClassName(), $methodName, $allowedCallers),
+                $this->errorMessage($methodReflection, $allowedCallers),
             ];
         }
 
@@ -138,7 +129,7 @@ final class AllowOnlyFromRule implements Rule
         // is one of the allowed callers
         if ($scope->isInClass() && !\in_array($scope->getClassReflection()->getName(), $allowedCallers)) {
             return [
-                $this->errorMessage($caller->getClassName(), $methodName, $allowedCallers),
+                $this->errorMessage($methodReflection, $allowedCallers),
             ];
         }
 
